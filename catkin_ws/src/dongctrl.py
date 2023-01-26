08b7bfd5f589c017a50181de2e2e3e2a9479983c
root <oksally@cu.ac.kr>
2023-01-26 19:41:37 +0900

    혜원이 객체 탐지 및 크기 판별 코드 결합( 다중 객체 인식 불가능)

--- catkin_ws/src/dongctrl.py
+++ catkin_ws/src/dongctrl.py
@@ -1,22 +1,72 @@
-import time
-from threading import Thread
-from test_package.hello import Hello
-from moraictrl import moraiCtrl
+import pyqtgraph as pg
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject, Qt, QThread, QTimer
+from PyQt5.QtGui import *
+
+import time, random
 import rospy
 import ros_numpy
+from threading import Thread
+
+from test_package.hello import Hello
+from moraictrl import moraiCtrl
+
+from morai_msgs.msg import CtrlCmd
 import numpy as np
 #from ackermann_msgs.msg import AckermannDriveStamped
 from sensor_msgs.msg import LaserScan , PointCloud2, PointField
 from sensor_msgs import point_cloud2
 from std_msgs.msg import Float64 , Header
 #from hunterctrl import hunterCtrl
+
 import math
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KDTree
+from sklearn.cluster import DBSCAN
+import pcl
+from sklearn.linear_model import RANSACRegressor
 
 
 class Car_Follower():
     def __init__(self):
         self.sub3D = rospy.Subscriber("/lidar3D", PointCloud2, self.callback3D)
         self.lidar_pub3D = rospy.Publisher("/refined_lidar3D", PointCloud2, queue_size=10)  # velodyne
+        self.send = rospy.Publisher("/lp_ctrl", CtrlCmd, queue_size=1)
+        self.ALGO_FLAG = 2  # 1 : kdtree. 2 : dbscan
+        self.pos = None
+
+        # object 출력용
+        self.objs = list()  # for display to graph
+
+        # object 출력용 position과 size
+        self.objsPos = list()
+        self.objsSize = list()
+
+        numofobjs = 150
+        for i in range(numofobjs):
+            pos = [0, 0, 0]  # x, y, z
+            size = [0, 0, 0]  # w, h, depth
+            self.objsPos.append(pos)
+            self.objsSize.append(size)
+
+    def kdtree(self, points):
+        kdt = KDTree(points, leaf_size=40)
+        # cluster_list = [[0 for j in range(0, )] for i in range(3)]
+        cluster_list = [0 for i in range(len(points))]
+        cluster = 1
+        for i in range(3):
+            cluster = cluster + 1
+            random_point = random.randrange(len(points))
+            # dist, ind = kdt.query(points[random_point:random_point+1], k=10)
+            ind = kdt.query_radius(points[random_point:random_point + 1], r=1)[0]
+            # print(ind)
+            for j in ind:
+                cluster_list[j] = cluster
+        self.clusterLabel = np.asarray(cluster_list)
+
+    def dbscan(self, points):  # dbscan eps = 1.5, min_size = 60
+        dbscan = DBSCAN(eps=0.5, min_samples=10, algorithm='ball_tree').fit(points)
+        self.clusterLabel = dbscan.labels_
 
     def callback3D(self, msg):
         scan = PointCloud2()
@@ -30,6 +80,7 @@ class Car_Follower():
         scan.data = msg.data
         scan.is_dense = msg.is_dense
 
+        # 좌표
         pc = ros_numpy.numpify(scan)
         points = np.zeros((pc.shape[0], 3))
 
@@ -37,7 +88,7 @@ class Car_Follower():
         points[:, 1] = pc['y']
         points[:, 2] = pc['z']
 
-        roi = {"x": [-5,5], "y": [-5, 5], "z": [-0.69, 20]}  # z값 수정, X 값 수정으로 전,후방 범위 조절
+        roi = {"x": [-5, 5], "y": [-5, 5], "z": [-0.69, 20]}  # z값 수정, X 값 수정으로 전,후방 범위 조절
 
         x_range = np.logical_and(points[:, 0] >= roi["x"][0], points[:, 0] <= roi["x"][1])
         y_range = np.logical_and(points[:, 1] >= roi["y"][0], points[:, 1] <= roi["y"][1])
@@ -46,7 +97,6 @@ class Car_Follower():
         pass_through_filter = np.where(np.logical_and(x_range, np.logical_and(y_range, z_range)) == True)[0]
         points = points[pass_through_filter, :]
         # points[:, 0] = x축 값 , points[:, 1] = y축 값 , points[:, 2] = z축 값
-
         #z축 값 추출 1차원 배열로 재배열
         # Zaxis=points[:,2].reshape(-1)
 
@@ -79,72 +129,57 @@ class Car_Follower():
             xy.append(re_yaxis[i]/re_xaxis[i])
             xypoint.append(math.sqrt((re_xaxis[i]**2)+(re_yaxis[i]**2)))
         if min(xypoint) < 1.5:
-            keyinput()
+            passw
 
         print("최소 기울기 ", min(xy))
         print("최대 기울기 ", max(xy))
         print("최소 거리 ", min(xypoint))
         print("최대 거리 ", max(xypoint))
 
+        if self.ALGO_FLAG == 1:
+            self.kdtree(points)
+        elif self.ALGO_FLAG == 2:
+            self.dbscan(points)
+
+        for i in range(1, max(self.clusterLabel)+1):
+            tempobjPos = self.objsPos[i]
+            tempobjSize = self.objsSize[i]
+
+            index = np.asarray(np.where(self.clusterLabel == i))
+            # print(i, 'cluster 개수 : ', len(index[0]))
+            x = np.min(points[index, 0])
+            y = np.min(points[index, 1])
+            x_size = np.max(points[index, 0]) - np.min(points[index, 0])  # x_max 3
+            y_size = np.max(points[index, 1]) - np.min(points[index, 1])  # y_max 1.3
+
+            # car size bounding box
+            carLength = 9 # 경차 : 3.6 소형 : 4.7 화물 차량 : 9
+            carHeight = 1 # 경차 : 2 소형 : 2 화물 차량 : 9
+            if (x_size <= carLength+1) and (y_size <= carHeight+1):
+                tempobjPos[0] = x
+                tempobjPos[1] = y
+                tempobjSize[0] = x_size
+                tempobjSize[1] = y_size
+                print("%d : [%.2f, %.2f, %.2f, %.2f]" % (i, tempobjPos[0], tempobjPos[1], tempobjSize[0], tempobjSize[1]))
+
+        steering = 0
+        # print(steering, center_dist, left_curv, right_curv)
+
+
+        send = CtrlCmd()
+        send.velocity = 0
+        send.steering = steering
 
         header = Header()
         header.frame_id = "velodyne"
         scan = point_cloud2.create_cloud_xyz32(header, points)
         scan.header.stamp = rospy.Time.now()
         self.lidar_pub3D.publish(scan)
-
-    def keyinput(id, moraictl:moraiCtrl):
-        """def keyinput(id, moraictl:moraiCtrl)에서
-        moraictl:moraiCtrl은 파이썬 주석(Function annotation)이라 하며
-        앞 부분의 메개변수에 대한 주석 따라서, moraictl은 moraiCtrl이라고 설명 해주는 부분
-        """
-        flag = True
-        while flag:
-            inpdata = input('input value: ').split()
-            command = int(inpdata[0])
-            value = int(inpdata[1])
-
-            if command == -1:
-                flag = False
-            elif command == 0:
-                moraictl.emergencyBrake(True)
-                moraictl.setTargetSpeed(0)
-            elif command == 1:
-                moraictl.emergencyBrake(False)
-                moraictl.setTargetSpeed(value)
-            elif command == 2:
-                moraictl.setSteeringAngle(value)
+        self.send.publish(send)
 
 
 if __name__ == '__main__':
     mct = moraiCtrl()
     Car_Follower()
     #mct = hunterCtrl()
-    th1 = Thread(target=Car_Follower.keyinput, args=(1, mct))
-    th1.start()
     mct.runCtrl()
-    th1.join()
-    print('process is all done')
-
-
-    '''
-        def keyinput(id, moraictl:moraiCtrl):
-        flag = True
-        while flag:
-            inpdata = input('input value: ').split()
-            command = int(inpdata[0])
-            value = int(inpdata[1])
-
-            if command == -1:
-                flag = False
-            elif command == 0:
-                moraictl.emergencyBrake(True)
-                moraictl.setTargetSpeed(0)
-            elif command == 1:
-                moraictl.emergencyBrake(False)
-                moraictl.setTargetSpeed(value)
-            elif command == 2:
-                moraictl.setSteeringAngle(value)
-
-
-        print('keyinput has been died') '''
